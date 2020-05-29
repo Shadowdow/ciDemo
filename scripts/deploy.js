5e560e4014427f711629d48e9f414abb822a515c
shadow <yingzhang@thoughtworks.com>
2020-05-29 17:46:56 +0800

    update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,7 +14,17 @@ const projectPath = path.resolve(path.dirname(''), 'dist');
 const weixinCli =
   process.env.WEIXIN_CLI ||
   '/Applications/wechatwebdevtools.app/Contents/MacOS/cli';
-const loginCommand = `${weixinCli} -l`;
+const loginCommand = `miniprogram-ci \\
+  preview \\
+  --pp ${projectPath} \\
+  --pkp ./private.key \\
+  --appid wx3104fa42162177c0 \\
+  --uv 1.0.0 \\
+  -r 1 \\
+  --enable-es6 true \\
+  --qrcode-format image \\
+  --qrcode-output-dest './scripts/x.jpg' \\`
+
 const deployCommand = `${weixinCli} -u ${VERSION}@${projectPath}`;
 const gitCommand = 'git log --pretty=\'%s\' -1';
 
@@ -67,33 +77,33 @@ function deploy(commitMsg) {
   });
 }
 
-// getLatestCommitMsg(function (commitMsg) {
-//   login(commitMsg);
-// });
+getLatestCommitMsg(function (commitMsg) {
+  login(commitMsg);
+});
 
-const ci = require('miniprogram-ci');
-(async () => {
-  const project = new ci.Project({
-    appid: 'wx3104fa42162177c0',
-    type: 'miniProgram',
-    projectPath: projectPath,
-    privateKeyPath: path.resolve(path.dirname(''), 'scripts/private.key'),
-    ignores: ['node_modules/**/*'],
-  });
-  ci.preview({
-    project,
-    desc: 'hello',
-    setting: {
-      es6: true,
-    },
-    qrcodeFormat: 'image',
-    qrcodeOutputDest: path.resolve(path.dirname(''), 'qrcode/destination.jpg'),
-    onProgressUpdate: console.log,
-    pagePath: 'pages/index/index', // 预览页面
-    // searchQuery: 'a=1&b=2',  // 预览参数 [注意!]这里的`&`字符在命令行中应写成转义字符`\&`
-  }).then((res) => {
-    console.log('res', res);
-  }, (err) => {
-    console.log('err', err);
-  });
-})();
+// const ci = require('miniprogram-ci');
+// (async () => {
+//   const project = new ci.Project({
+//     appid: 'wx3104fa42162177c0',
+//     type: 'miniProgram',
+//     projectPath: projectPath,
+//     privateKeyPath: path.resolve(path.dirname(''), 'scripts/private.key'),
+//     ignores: ['node_modules/**/*'],
+//   });
+//   ci.preview({
+//     project,
+//     desc: 'hello',
+//     setting: {
+//       es6: true,
+//     },
+//     qrcodeFormat: 'image',
+//     qrcodeOutputDest: path.resolve(path.dirname(''), 'qrcode/destination.jpg'),
+//     onProgressUpdate: console.log,
+//     pagePath: 'pages/index/index', // 预览页面
+//     // searchQuery: 'a=1&b=2',  // 预览参数 [注意!]这里的`&`字符在命令行中应写成转义字符`\&`
+//   }).then((res) => {
+//     console.log('res', res);
+//   }, (err) => {
+//     console.log('err', err);
+//   });
+// })();
